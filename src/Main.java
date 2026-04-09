0f1c6b4114246ece6c887eff9a92d77f2007bd0c
Tomas Rivera <tr4462603@gmail.com>
2026-04-09 13:57:43 -0400

    main: Add execution path for compiler

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,33 @@ import org.antlr.v4.runtime.tree.ParseTree;
 public class Main{
 
     public static void main(String[] args) throws Exception{
-        if (args.length != 1) {
-            throw new RuntimeException("Invalid number of arguments provided. Please provide the file to run.");
+        if (args.length != 2) {
+            throw new RuntimeException("Invalid number of arguments provided. " +
+                                       "Please provide the execution mode [compile | interpret] and the target file.");
         }
 
-        CharStream input = CharStreams.fromFileName(args[0]);
+        CharStream input = CharStreams.fromFileName(args[1]);
 
         DelphiLexer lexer = new DelphiLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         DelphiParser parser = new DelphiParser(tokens);
 
         ParseTree tree = parser.program();
-
-        DelphiInterpreter interpreter = new DelphiInterpreter();
-        try {
-            interpreter.visit(tree);
-        } catch (delphiRuntimeError e) {
-            System.err.println("delphi Runtime Error: " + e.getMessage());
+        
+        if (args[0].equals("interpret")) {
+            DelphiInterpreter interpreter = new DelphiInterpreter();
+            try {
+                interpreter.visit(tree);
+            } catch (delphiRuntimeError e) {
+                System.err.println("delphi Runtime Error: " + e.getMessage());
+            }
         }
-
-
+        else if (args[0].equals("compile")) {
+            // compile here
+        }
+        else {
+            throw new RuntimeException("Bad execution mode provided.");
+        }
     }
-
-
 
 }
