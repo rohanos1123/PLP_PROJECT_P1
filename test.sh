14f8586d6d48519a4e85f757ee8aa77edc4136c9
Tomas Rivera <tr4462603@gmail.com>
2026-04-29 16:06:25 -0400

    test: remove interpreter from test script

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-for i in {0..18}; do
+for i in {0..28}; do
     clang -lm -w -O0 output/test$i.ll -o out && ./out
-    java -cp lib/antlr-4.13.2-complete.jar:bin Main interpret tests/test$i.pas
 done
